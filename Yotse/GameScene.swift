80a3fe032ff50b37280a79e42d8bf7e4ce1d9a4e
Jerad Rose <jader@jader201.com>
2015-04-02 21:35:37 -0400

    Messing w/ physics; Cleaning up code

--- Yotse/GameScene.swift
+++ Yotse/GameScene.swift
@@ -14,33 +14,46 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
         self.backgroundColor = UIColor.whiteColor()
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        self.physicsWorld.gravity = CGVectorMake(0.0, -1)
+        self.physicsWorld.gravity = CGVectorMake(0.0, -5)
+
+        println(self.physicsWorld.speed)
+        self.physicsWorld.speed = 4.0
     }
 
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-
-            let sprite = SKSpriteNode(imageNamed:"Dice_1")
-
-            sprite.position = location
-
-            sprite.physicsBody = SKPhysicsBody(rectangleOfSize: sprite.size)
-            sprite.physicsBody!.dynamic = true
-
-            let rollAction = SKAction.runBlock({
-                sprite.runAction(SKAction.setTexture(self.textureAtlas.textureNamed("Dice_\(self.rollDice())")))
-            })
-
-            let roll = SKAction.group([rollAction])
-            let wait = SKAction.waitForDuration(0.1)
-
-            let sequence = SKAction.sequence([roll, wait])
-            let repeat = SKAction.repeatActionForever(sequence)
-            sprite.runAction(repeat)
-
-            self.addChild(sprite)
+            addDice(touch.locationInNode(self))
         }
+    }
+
+    func addDice(position: CGPoint) {
+        let sprite = SKSpriteNode(imageNamed:"Dice_1")
+
+        sprite.position = position
+
+        sprite.physicsBody = SKPhysicsBody(texture: textureAtlas.textureNamed("Dice_1"), size: sprite.size)
+        sprite.physicsBody!.dynamic = true
+        println("density = \(sprite.physicsBody!.density)")
+        println("restitution = \(sprite.physicsBody!.restitution)")
+        println("friction = \(sprite.physicsBody!.friction)")
+
+        sprite.physicsBody!.mass = 500
+        sprite.physicsBody!.restitution = 0.5
+        sprite.physicsBody!.friction = 0.2
+
+        let rollAction = SKAction.runBlock({
+            sprite.runAction(SKAction.setTexture(self.textureAtlas.textureNamed("Dice_\(self.rollDice())")))
+        })
+
+        let roll = SKAction.group([rollAction])
+        let wait = SKAction.waitForDuration(0.1)
+
+        let sequence = SKAction.sequence([roll, wait])
+        let repeat = SKAction.repeatActionForever(sequence)
+        sprite.runAction(repeat)
+
+        self.addChild(sprite)
+
     }
 
     func rollDice() -> String {
